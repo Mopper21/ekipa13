d92740e5198cd286b888bb5ed14ef3aa7d83bf7b
Mopper21 <152798669+Mopper21@users.noreply.github.com>
2024-01-23 19:25:06 +0100

    Add files via upload

--- Projekt/springboot/src/main/java/com/projekt/controllers/PacientController.java
+++ Projekt/springboot/src/main/java/com/projekt/controllers/PacientController.java
@@ -3,9 +3,12 @@ package com.projekt.controllers;
 import com.projekt.dao.*;
 import com.projekt.models.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/pacienti")
@@ -20,10 +23,17 @@ public class PacientController {
         return PacientRepository.findAll();
     }
 
+    @GetMapping("/{pacientId}")
+    public Optional<Pacient> getPacientById(@PathVariable Long pacientId) {
+        return PacientRepository.findById(pacientId);
+    }
+
+
     @PostMapping("/register")
     public Pacient registerPacient(@RequestBody Pacient pacient) {
         return PacientRepository.save(pacient);
     }
+
 
     @GetMapping("/{pacientId}/appointments")
     public Iterable<Napotnice> getPacientAppointments(@PathVariable Long pacientId) {
@@ -48,4 +58,58 @@ public class PacientController {
             return null;  // or throw an exception, return an error response, etc.
         }
     }
+
+    @PutMapping("/{pacientId}")
+    public ResponseEntity<Pacient> updatePacient(@PathVariable Long pacientId, @RequestBody Pacient updatedPacient) {
+        // Assume PacientRepository has a method to find by ID
+        Pacient existingPacient = PacientRepository.findById(pacientId).orElse(null);
+
+        if (existingPacient != null) {
+            // Update fields of existingPacient with fields from updatedPacient
+            existingPacient.setIme(updatedPacient.getIme());
+            existingPacient.setDatumRojstva(updatedPacient.getDatumRojstva());
+
+            // Save the updated pacient
+            Pacient updatedPacientEntity = PacientRepository.save(existingPacient);
+
+            return ResponseEntity.ok(updatedPacientEntity);
+        } else {
+            // If pacient with given ID is not found, return 404 (Not Found)
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @DeleteMapping("/{pacientId}")
+    public ResponseEntity<Void> deletePacient(@PathVariable Long pacientId) {
+        // Assume PacientRepository has a method to find by ID
+        Pacient existingPacient = PacientRepository.findById(pacientId).orElse(null);
+
+        if (existingPacient != null) {
+            // Delete the pacient
+            PacientRepository.deleteById(pacientId);
+
+            return ResponseEntity.noContent().build();
+        } else {
+            // If pacient with given ID is not found, return 404 (Not Found)
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @GetMapping("/findByImeAndDatumRojstva")
+    public Iterable<Pacient> findByImeAndDatumRojstva(
+            @RequestParam String ime,
+            @RequestParam String datumRojstva) {
+        return PacientRepository.findByImeAndDatumRojstva(ime, datumRojstva);
+    }
+
+    @GetMapping("/byImeAndPriimekAndNaslov")
+    public ResponseEntity<Iterable<Pacient>> findPacientByImeAndPriimekAndNaslov(
+            @RequestParam String ime,
+            @RequestParam String priimek,
+            @RequestParam String naslov) {
+        Iterable<Pacient> pacients = PacientRepository.findPacientByImeAndPriimekAndNaslov(ime, priimek, naslov);
+        return ResponseEntity.ok(pacients);
+    }
+
 }
+
--- Projekt/springboot/src/main/java/com/projekt/dao/PacientRepository.java
+++ Projekt/springboot/src/main/java/com/projekt/dao/PacientRepository.java
@@ -13,4 +13,11 @@ public interface PacientRepository extends CrudRepository<Pacient, Long> {
 
     @Query("SELECT p FROM Pacient p WHERE p.ime LIKE %?1% OR p.priimek LIKE %?1%")
     List<Pacient> searchByName(String name);
+
+    Iterable<Pacient> findByParameter1AndParameter2(String parameter1, String parameter2);
+
+    Iterable<Pacient> findByImeAndDatumRojstva(String ime, String datumRojstva);
+
+
+    Iterable<Pacient> findPacientByImeAndPriimekAndNaslov(String ime, String priimek, String naslov);
 }
