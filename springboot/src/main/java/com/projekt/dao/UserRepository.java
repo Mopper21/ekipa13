7242c2aa790c709e7fa8c7c87eddd633cfa242d7
Julio Mati <152798669+Mopper21@users.noreply.github.com>
2024-06-20 13:28:19 +0200

    Delete springboot directory

--- springboot/src/main/java/com/projekt/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projekt.dao;
-
-import com.projekt.models.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.*;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-}
